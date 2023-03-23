62f954f15dab6dca6acc44c69b8482a35a93a7b4
Gabriel Torres Brum <dev.gabriel.torres@gmail.com>
2023-03-23 16:07:31 -0300

    feat: changing throttle middleware message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,5 +45,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ThrottleRequestsException) {
+            return response_no(code: 429, message: __("custom.too-many-requests"));
+        }
+
+        return parent::render($request, $exception);
     }
 }
