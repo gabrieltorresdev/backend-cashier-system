552f9514d81ada2ba38878bafbbe657652c95c18
Gabriel Torres Brum <dev.gabriel.torres@gmail.com>
2023-03-22 22:14:43 -0300

    feat: adding new helpers for next features

--- app/Helpers/index.php
+++ app/Helpers/index.php
@@ -5,12 +5,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
 
 if (!function_exists('response_ok')) {
     function response_ok(int $code = 200, array $data = [], string $message = ""): JsonResponse
     {
         if (empty($message))
-            $message = __('custom.response-success-message');
+            $message = __('custom.common.response-success-message');
 
         $response = [
             'message' => $message,
@@ -25,7 +26,7 @@ if (!function_exists('response_no')) {
     function response_no(int $code = 500, array $errors = [], string $message = "", array $data = []): JsonResponse
     {
         if (empty($message))
-            $message = __('custom.response-error-message');
+            $message = __('custom.common.response-error-message');
 
         $response = [
             'message' => $message,
@@ -138,5 +139,82 @@ if (!function_exists('verification_token')) {
         $string = \Illuminate\Support\Str::random($length);
 
         return str($string)->upper()->value();
+    }
+}
+
+if (!function_exists('format_number')) {
+    /**
+     * Format a number
+     * @param float|int $length
+     */
+    function format_number(float|int $number, int $decimals = 2): string
+    {
+        return number_format($number, $decimals, '.', '');
+    }
+}
+if (!function_exists('merge_arrays')) {
+    function merge_arrays(array $arr1, array $arr2, string $key = "id")
+    {
+        $merged = [];
+
+        foreach ($arr1 as $item1) {
+            $found = false;
+            foreach ($arr2 as $item2) {
+                $match = true;
+                if (is_array($key)) {
+                    foreach ($key as $k) {
+                        if ($item1[$k] !== $item2[$k]) {
+                            $match = false;
+                            break;
+                        }
+                    }
+                } else {
+                    $match = $item1[$key] === $item2[$key];
+                }
+                if ($match) {
+                    $merged[] = array_merge($item1, $item2);
+                    $found = true;
+                    break;
+                }
+            }
+            if (!$found) {
+                $merged[] = $item1;
+            }
+        }
+
+        return array_merge($merged, array_udiff($arr2, $merged, function ($a, $b) use ($key) {
+            if (is_array($key)) {
+                $cmp = 0;
+                foreach ($key as $k) {
+                    $cmp = strcmp($a[$k], $b[$k]);
+                    if ($cmp !== 0) {
+                        break;
+                    }
+                }
+                return $cmp;
+            } else {
+                return strcmp($a[$key], $b[$key]);
+            }
+        }));
+    }
+}
+
+if (!function_exists('throw_exception')) {
+    /**
+     * @throws Exception
+     */
+    function throw_exception(string $message = "", int $code = 500, string $exception = \Exception::class)
+    {
+        throw new $exception($message, $code);
+    }
+}
+
+if (!function_exists('throw_validation_exception')) {
+    /**
+     * @throws ValidationException
+     */
+    function throw_validation_exception(array $messages)
+    {
+        throw ValidationException::withMessages($messages);
     }
 }
